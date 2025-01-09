66aa635aafc586647d25a270c6d82ceb1123bb1f
mdelgadonyc <moises.f.delgado@gmail.com>
2025-01-09 13:32:56 -0500

    feat: implement Point and Line drawing functionality

--- graphics.py
+++ graphics.py
@@ -1,13 +1,34 @@
 from tkinter import Tk, BOTH, Canvas
 
+class Points:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y     
+
+
+class Line:
+    def __init__(
+            self,
+            p1,
+            p2
+    ):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas: Canvas, fill_color="black"):
+        canvas.create_line(
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
+            )
+
+
 class Window:
     def __init__(self, width: int, height: int):
         self.__root = Tk()
         self.__root.title("Mazing")
-        self.canvas = Canvas(self.__root, width=width, height=height, bg="white")        
-        self.canvas.pack(fill=BOTH, expand=1)
-        self.is_running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__canvas = Canvas(self.__root, width=width, height=height, bg="white")        
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.is_running = False
 
 
     def redraw(self):
@@ -22,5 +43,14 @@ class Window:
         print("window closed...")
 
 
+    def draw_line(self, line: Line, color="black"):
+        line.draw(self.__canvas, color)
+ 
+
     def close(self):
         self.__running = False
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from graphics import Window
+from graphics import Window, Line, Points
+
 
 def main():
     win = Window(800, 600)
+    l = Line(Points(100, 100), Points(400, 400))
+    win.draw_line(l, "black")
     win.wait_for_close()
+
+main()
